1ea65b6c8ccb6d2e69b31a20f2352815ce130ab5
Efehan Turhan <94855256+pandarelax@users.noreply.github.com>
2024-02-10 00:30:34 +0300

    Update role badges for usermanagement page

--- web/src/components/dashboard/users-management/UserCountSection.tsx
+++ web/src/components/dashboard/users-management/UserCountSection.tsx
@@ -1,33 +1,25 @@
-import UserCountCard from './UserCountCard';
+import { FaUser, FaUserShield } from 'react-icons/fa';
 import { IAuthUser, RolesEnum } from '../../../types/auth.types';
-import { FaUser, FaUserCog, FaUserShield, FaUserTie } from 'react-icons/fa';
+import UserCountCard from './UserCountCard';
 
 interface IProps {
   usersList: IAuthUser[];
 }
 
 const UserCountSection = ({ usersList }: IProps) => {
-  let owners = 0;
   let admins = 0;
-  let managers = 0;
   let users = 0;
 
   usersList.forEach((item) => {
-    if (item.roles.includes(RolesEnum.OWNER)) {
-      owners++;
-    } else if (item.roles.includes(RolesEnum.ADMIN)) {
+    if (item.roles.includes(RolesEnum.ADMIN)) {
       admins++;
-    } else if (item.roles.includes(RolesEnum.MANAGER)) {
-      managers++;
-    } else if (item.roles.includes(RolesEnum.USER)) {
+    }  else if (item.roles.includes(RolesEnum.USER)) {
       users++;
     }
   });
 
   const userCountData = [
-    { count: owners, role: RolesEnum.OWNER, icon: FaUserCog, color: '#3b3549' },
     { count: admins, role: RolesEnum.ADMIN, icon: FaUserShield, color: '#9333EA' },
-    { count: managers, role: RolesEnum.MANAGER, icon: FaUserTie, color: '#0B96BC' },
     { count: users, role: RolesEnum.USER, icon: FaUser, color: '#FEC223' },
   ];
 
